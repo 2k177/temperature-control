efb3153b1d0702cecac798851e28dfba89fd25fe
Danusha U <danuu@qti.qualcomm.com>
2023-05-09 11:46:58 +0530

    Update simple temperature counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [temperature, setTemperature] = useState(10);
+  const increaseTemperature = () => {
+    const newTemperature = temperature + 1;
+    setTemperature(newTemperature);
+  };
+  const decreaseTemperature = () => {
+    const newTemperature = temperature - 1;
+    setTemperature(newTemperature);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Temperature control App</h1>
+      <button onClick={increaseTemperature}>+</button>
+      <button onClick={decreaseTemperature}>-</button>
+      <h4>{temperature}</h4>
     </div>
   );
 }
